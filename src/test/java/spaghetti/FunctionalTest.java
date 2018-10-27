7792c9bcdb4d43283eed79408c4d4244cc94c036
Denys Shynkarenko <denys_shynkarenko1@epam.com>
2018-10-27 22:28:11 +0300

    update

--- src/test/java/spaghetti/FunctionalTest.java
+++ src/test/java/spaghetti/FunctionalTest.java
@@ -1,198 +1,176 @@
 package spaghetti;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-/**
- * Automate following steps: Goto www.nascar.com Register Login with the registered credentials Logout
- *
- */
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FunctionalTest {
 
     private WebDriver driver;
     private WebDriverWait driverWait;
 
-    private static final String urlMainPage = "http://www.nascar.com/en_us/sprint-cup-series.html";
+    private static final String MAIN_PAGE_URL = "https://www.olx.ua/";
 
-    private static final By hrefMyProfile = By.xpath("//ul[@class='primary active']//a[@id='myProfileLink']");
-    private static final By hrefLogin = By.xpath("//a[@class='gigyaLoginDialog tab tab-button']");
-    private static final By hrefRegister = By
-            .xpath("//div[@id='gigya-login-screen']//a[@data-switch-screen='gigya-register-screen']");
-    private static final By buttonLogOut = By
-            .xpath("//div[@class='pageContainer']//input[@class='gigya-input-submit logout']");
+    private static final By LOGO_LINK = By.id("headerLogo");
+    private static final By SEARCH_INPUT = By.id("headerSearch");
+    private static final By SEARCH_BUTTON = By.id("submit-searchmain");
+    private static final By FOUND_ITEMS_LABELS = By.xpath("//a[contains(@class, 'detailsLink')]/strong");
 
-    private static final By dialog = By.xpath("//div[@class='gigya-screen-dialog-inner']");
-    private static final By textUser = dialog.name("username");
-    private static final By password = dialog.name("password");
-    private static final By submit = dialog.xpath("//input[@class='gigya-input-submit']/..");
+    private static final By PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON = By.xpath("//span[@class='highlight-close']");
+    private static final By GEO_SUGGESTIONS_CLOSE_BUTTON = By.id("geo-suggestions-close");
+    private static final By COOKIES_BAR_CLOSE_BUTTON = By.xpath("//a[contains(@class, 'cookiesBarClose')][contains(@class, 'close')]");
+    private static final By ITEM_LABEL = By.xpath("//h1");
 
-    private static final By textEmail = By.xpath("//div[@class='gigya-screen-dialog-inner']//input[@name='email']");
-    private static final By textZIP = dialog.name("zip");
-    private static final By checkboxPrivacyPolicy = By
-            .xpath("//div[@class='gigya-screen-dialog-inner']//input[@name='data.terms']");
+    private static final String CATEGORY_LINK_XPATH_LOCATOR = "//a[@data-id][..//span[text()='%s']]";
 
-    private static final By divRaceCenter = By.xpath("//div[@class='race-center-container clearfix ']");
-    private static final By aEntryList = By.xpath("//div[@id='tab-1']//div[@class='button']/a");
+    private static final By CHANGE_LANGUAGE_LINK = By.id("changeLang");
 
-    @BeforeClass
-    public void beforeTestClass() {
-        System.setProperty("webdriver.gecko.driver",
-                getClass().getClassLoader().getResource("geckodriver/geckodriver.exe").getPath());
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
-
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driverWait = new WebDriverWait(driver, 30);
+    @BeforeMethod
+    public void beforeTestMethod() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1700, 850));
+        driverWait = new WebDriverWait(driver, 20);
     }
 
-    @AfterClass
-    public void afterTestClass() {
-        driver.quit();
+    @AfterMethod
+    public void afterTestMethod() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
-    public void loginByRegisteredUser() {
+    public void checkSearchingProductByName() {
         // test data
-        String registeredUserEmail = "shynkarenkoden@yandex.ru";
-        String registeredUserPassword = "Denys123456789";
+        String productName = "Lenovo L520";
 
-        // Open page
-        driver.get(urlMainPage);
-        String originalHandle = driver.getWindowHandle();
+        // Open Main page
+        driver.get(MAIN_PAGE_URL);
         waitForJSandJQueryToLoad();
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(divRaceCenter));
-        System.out.println(driver.getTitle());
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(LOGO_LINK));
 
-        // Go to login
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(aEntryList));
-        driver.findElement(aEntryList).click();
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(COOKIES_BAR_CLOSE_BUTTON));
+        driver.findElement(COOKIES_BAR_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(COOKIES_BAR_CLOSE_BUTTON));
+
+        // Search a item
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(SEARCH_INPUT));
+        driver.findElement(SEARCH_INPUT).sendKeys(productName);
         waitForJSandJQueryToLoad();
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(hrefLogin));
-        driver.findElement(hrefLogin).click();
-
-        waitForJSandJQueryToLoad();
-
-        // Login as registered user
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(dialog));
-        driver.findElement(textUser).sendKeys(registeredUserEmail);
-        driver.findElement(password).sendKeys(registeredUserPassword);
-        driver.findElement(submit).submit();
-        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(submit));
-
-        waitForJSandJQueryToLoad();
-        closeTabs(originalHandle);
-
-        // Go to user profile
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(hrefMyProfile));
-        driver.findElement(hrefMyProfile).click();
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(SEARCH_BUTTON));
+        driver.findElement(SEARCH_BUTTON).click();
 
         waitForJSandJQueryToLoad();
 
-        // Logout
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(buttonLogOut));
-        driver.findElement(buttonLogOut).click();
-        closeTabs(originalHandle);
+        // Open Product Details page of a first matched found item
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(FOUND_ITEMS_LABELS));
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON));
+        driver.findElement(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON));
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(GEO_SUGGESTIONS_CLOSE_BUTTON));
+        driver.findElement(GEO_SUGGESTIONS_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(GEO_SUGGESTIONS_CLOSE_BUTTON));
+
+        List<WebElement> items = driver.findElements(FOUND_ITEMS_LABELS).stream()
+                .filter(item -> item.getText().contains(productName))
+                .collect(Collectors.toList());
+        assertThat(items.size()).as("Check number of matched found items").isGreaterThan(0);
+
+        String productNameOnSearchPage = items.get(0).getText();
+        items.get(0).click();
+
+        waitForJSandJQueryToLoad();
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(ITEM_LABEL));
+        assertThat(driver.findElement(ITEM_LABEL).getText())
+                .as("Check product names are the same on Details and Search pages")
+                .isEqualTo(productNameOnSearchPage);
     }
 
     @Test
-    public void registerNewUser() {
+    public void checkSearchingProductByCategory() {
         // test data
-        String newUserEmail = "myemail@gmail.com";
-        String newUserPassword = "MySuperPass_word";
-        String newUserZip = "40211";
+        String categoryName = "Електроніка";
+        String subCategoryName = "Ноутбуки та аксесуари";
+        String productName = "Ноутбук";
 
-        // Open page
-        driver.get(urlMainPage);
+        // Open Main page
+        driver.get(MAIN_PAGE_URL);
         waitForJSandJQueryToLoad();
-        String originalHandle = driver.getWindowHandle();
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(divRaceCenter));
-        System.out.println(driver.getTitle());
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(LOGO_LINK));
 
-        // Go to login
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(aEntryList));
-        driver.findElement(aEntryList).click();
-        waitForJSandJQueryToLoad();
-        closeTabs(originalHandle);
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(hrefLogin));
-        waitForJSandJQueryToLoad();
-        driver.findElement(hrefLogin).click();
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(COOKIES_BAR_CLOSE_BUTTON));
+        driver.findElement(COOKIES_BAR_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(COOKIES_BAR_CLOSE_BUTTON));
 
-        waitForJSandJQueryToLoad();
+        // Change language
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(CHANGE_LANGUAGE_LINK));
+        driver.findElement(CHANGE_LANGUAGE_LINK).click();
 
-        // Open register page
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(hrefRegister));
-        driver.findElement(hrefRegister).click();
-
-        waitForJSandJQueryToLoad();
-
-        // Fill the fields
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(textEmail));
-        driver.findElement(textEmail).sendKeys(newUserEmail);
-        driver.findElement(password).sendKeys(newUserPassword);
-        driver.findElement(textZIP).sendKeys(newUserZip);
-        if (!driver.findElement(checkboxPrivacyPolicy).isSelected())
-            driver.findElement(checkboxPrivacyPolicy).click();
-
-        // Submit
-        driver.findElement(submit).click();
-        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(submit));
+        // Open category and sub-category
+        By category = By.xpath(String.format(CATEGORY_LINK_XPATH_LOCATOR, categoryName));
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(category));
+        driver.findElement(category).click();
+        By subCategory = By.xpath(String.format(CATEGORY_LINK_XPATH_LOCATOR, subCategoryName));
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(subCategory));
+        driver.findElement(subCategory).click();
 
         waitForJSandJQueryToLoad();
-        closeTabs(originalHandle);
 
-        // Go to user profile
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(hrefMyProfile));
-        driver.findElement(hrefMyProfile).click();
+        // Open Product Details page of a first matched found item
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(FOUND_ITEMS_LABELS));
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON));
+        driver.findElement(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(PRODUCT_WITH_DELIVERY_HIGHLIGHT_CLOSE_BUTTON));
+
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(GEO_SUGGESTIONS_CLOSE_BUTTON));
+        driver.findElement(GEO_SUGGESTIONS_CLOSE_BUTTON).click();
+        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(GEO_SUGGESTIONS_CLOSE_BUTTON));
+
+        List<WebElement> items = driver.findElements(FOUND_ITEMS_LABELS).stream()
+                .filter(item -> item.getText().contains(productName))
+                .collect(Collectors.toList());
+        assertThat(items.size()).as("Check number of matched found items").isGreaterThan(0);
+
+        String productNameOnSearchPage = items.get(0).getText();
+        items.get(0).click();
+
         waitForJSandJQueryToLoad();
 
-        // Logout
-        driverWait.until(ExpectedConditions.visibilityOfElementLocated(buttonLogOut));
-        driver.findElement(buttonLogOut).click();
-        driverWait.until(ExpectedConditions.invisibilityOfElementLocated(buttonLogOut));
-        closeTabs(originalHandle);
+        driverWait.until(ExpectedConditions.visibilityOfElementLocated(ITEM_LABEL));
+        assertThat(driver.findElement(ITEM_LABEL).getText())
+                .as("Check product names are the same on Details and Search pages")
+                .isEqualTo(productNameOnSearchPage);
     }
 
     private boolean waitForJSandJQueryToLoad() {
         // wait for jQuery to load
-        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver driver) {
-                try {
-                    return ((Long) ((JavascriptExecutor) driver).executeScript("return jQuery.active") == 0);
-                } catch (Exception e) {
-                    // no jQuery present
-                    return true;
-                }
-            }
-        };
+        ExpectedCondition<Boolean> jQueryLoad = jsExecutor -> ((Long) ((JavascriptExecutor) jsExecutor)
+                .executeScript("return jQuery.active")) == 0;
 
         // wait for Javascript to load
-        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
-            public Boolean apply(WebDriver driver) {
-                return ((JavascriptExecutor) driver).executeScript("return document.readyState").toString()
-                        .equals("complete");
-            }
-        };
+        ExpectedCondition<Boolean> jsLoad = jsExecutor -> ((JavascriptExecutor) jsExecutor)
+                .executeScript("return document.readyState").toString().equals("complete");
 
         return driverWait.until(jQueryLoad) && driverWait.until(jsLoad);
-    }
-
-    private void closeTabs(String exceptThisTab) {
-        for (String handle : driver.getWindowHandles()) {
-            if (!handle.equals(exceptThisTab)) {
-                driver.switchTo().window(handle);
-                driver.close();
-            }
-        }
-
-        driver.switchTo().window(exceptThisTab);
     }
 }
--- src/test/java/spaghetti/YahooSitesLoadTimeTest.java
+++ src/test/java/spaghetti/YahooSitesLoadTimeTest.java
@@ -1,21 +1,24 @@
 package spaghetti;
 
-import org.joda.time.LocalTime;
-import org.joda.time.Seconds;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
+import java.time.LocalTime;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 /**
  * Automate following scenario: Goto www.yahoo.com and fetch the Yahoo sites (Mail, Autos etc) in the Left dynamically
@@ -24,75 +27,69 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class YahooSitesLoadTimeTest {
 
+    private static final int EXPECTED_MAX_LOAD_TIME = 10;
+    private static final String URL_MAIN_PAGE = "https://www.yahoo.com/";
+
     private WebDriver driver;
 
-    private final int expectedMaxLoadTime = 7;
+    private Map<String, Long> loadTimeResults;
 
-    private Map<String, Integer> loadTimeResults;
-
-    private final String urlMainPage = "https://www.yahoo.com/";
-    private final By links = By.xpath("//a");
+    private final By allLinks = By.xpath("//a");
 
     public YahooSitesLoadTimeTest() {
-        loadTimeResults = new HashMap<String, Integer>();
+        loadTimeResults = new HashMap<>();
     }
 
     @BeforeClass
     public void beforeTestClass() {
-        System.setProperty("webdriver.gecko.driver",
-                getClass().getClassLoader().getResource("geckodriver/geckodriver.exe").getPath());
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver/chromedriver.exe");
-
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1700, 850));
     }
 
     @AfterClass
     public void afterTestClass() {
-        driver.quit();
+        if (driver != null) {
+            driver.quit();
+        }
     }
 
     @Test
     public void checkYahooSitesLoadTime() {
 
         // find out load time for main Yahoo site
-        // loadTimeResults.put(yahooPage.getUrlMainPage(), getSiteLoadTime(""));
-        driver.get(urlMainPage);
+        driver.get(URL_MAIN_PAGE);
 
-        // Get sites
+        // get sites
         String href;
-        List<String> yahooSites = new ArrayList<String>();
-        List<WebElement> elementList = driver.findElements(links);
+        Set<String> yahooSites = new HashSet<>();
+        List<WebElement> elements = driver.findElements(allLinks);
 
-        for (WebElement element : elementList) {
+        for (WebElement element : elements) {
             href = element.getAttribute("href");
-            if (href != null && !yahooSites.contains(href) && href.contains("yahoo.com") && !href.contains("#")
-                    && !href.contains("=") && !href.contains(".html") && !href.contains(".htm") && !href.contains("-")
-                    && !href.equals("https://www.yahoo.com/"))
+            if (href != null && href.contains("yahoo.com")
+                    && !href.contains("#") && !href.contains("=") && !href.contains(".html")
+                    && !href.contains(".htm") && !href.contains("-") && !href.equals(URL_MAIN_PAGE))
                 yahooSites.add(href);
         }
 
         // find out load time for each Yahoo site
-        for (String url : yahooSites)
-            loadTimeResults.put(url, getSiteLoadTime(url));
+        yahooSites.forEach(siteUrl -> loadTimeResults.put(siteUrl, getSiteLoadTime(siteUrl)));
 
-        // print test results
-        for (String key : loadTimeResults.keySet())
-            System.out.println(key + " was loaded in " + loadTimeResults.get(key) + " seconds");
-
-        // verify test results with expected time
-        for (String key : loadTimeResults.keySet())
-            assertThat(loadTimeResults.get(key)).as("Load time of " + key + " page")
-                    .isLessThanOrEqualTo(expectedMaxLoadTime);
+        // verify test results
+        SoftAssertions.assertSoftly(softly -> loadTimeResults.forEach((key, value) -> {
+            System.out.println(String.format("%s was loaded in %s seconds", key, value));
+            softly.assertThat(value).as("Load time of %s page", key)
+                    .isLessThanOrEqualTo(EXPECTED_MAX_LOAD_TIME);
+        }));
     }
 
-    private int getSiteLoadTime(String url) {
+    private long getSiteLoadTime(String url) {
         LocalTime startTime = LocalTime.now();
 
-        driver.get(url.isEmpty() ? urlMainPage : url);
+        driver.get(url.isEmpty() ? URL_MAIN_PAGE : url);
         LocalTime endTime = LocalTime.now();
 
-        return Seconds.secondsBetween(startTime, endTime).getSeconds();
+        return startTime.until(endTime, SECONDS);
     }
-
 }
